9c9d6534399eb93c4e209bed3f4f4130dbfa3d98
Fer <fjcesio@mail.austral.edu.ar>
2026-03-06 10:46:56 -0300

    initial commit

---
+++ src/main/java/com/lab1/nodevix/NodevixApplication.java
@@ -0,0 +1,13 @@
+package com.lab1.nodevix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodevixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodevixApplication.class, args);
+	}
+
+}
