45245e0f15c81014c338f03c6c1f1dddef4dd4ce
cuchud <cuchu.sere@gmail.com>
2026-05-19 11:06:22 -0300

    Added PhysicsEngine.tsx for sandbox simulation

---
+++ app/src/components/sandbox/modules/PhysicsEngine.ts
@@ -0,0 +1,53 @@
+import * as d3 from 'd3';
+import type { INode } from '../../../sandbox/interfaces';
+
+export type SimLink = { source: INode; target: INode; value: number };
+
+export class PhysicsEngine {
+  private simulation: d3.Simulation<INode, SimLink>;
+
+  constructor(nodes: INode[], links: SimLink[]) {
+    this.simulation = d3.forceSimulation<INode>(nodes)
+      .force('link', d3.forceLink<INode, SimLink>(links).distance(100))
+      .force('charge', d3.forceManyBody().strength(-200))
+      .force('center', d3.forceCenter(0, 0))
+      .force('collide', d3.forceCollide(30));
+
+    this.simulation.on('tick', () => {
+      nodes.forEach(n => {
+        n.pos.x = n.x ?? n.pos.x;
+        n.pos.y = n.y ?? n.pos.y;
+      });
+    });
+  }
+
+  onTick(callback: () => void): void {
+    const current = this.simulation.on('tick');
+    this.simulation.on('tick', () => {
+      if (typeof current === 'function') current();
+      callback();
+    });
+  }
+
+  restart(): void {
+    this.simulation.alpha(0.3).restart();
+  }
+
+  stop(): void {
+    this.simulation.stop();
+  }
+
+  updateNodes(nodes: INode[]): void {
+    this.simulation.nodes(nodes);
+    this.restart();
+  }
+
+  updateLinks(links: SimLink[]): void {
+    (this.simulation.force('link') as d3.ForceLink<INode, SimLink>).links(links);
+    this.restart();
+  }
+
+  getSimulation(): d3.Simulation<INode, SimLink> {
+    return this.simulation;
+  }
+}
